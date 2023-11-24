15d902009c40eb716fcf935842f4087dc78723f2
berkay <oziierdem59@gmail.com>
2023-11-24 10:45:33 +0300

    sepetekle

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-// const row = document.querySelector('.row')
 const span = document.querySelector('#span')
+// const row = document.querySelector('.row')
+
 
 let sepet = []
 
@@ -7,40 +8,46 @@ let localItem = localStorage.getItem('sepet')
 
 if (localItem) {
     sepet = JSON.parse(localItem)
+    // console.log(sepet)
     span.textContent = sepet.length
 }
-
-// console.log(urunler);
-
+// localStorage.clear()
+// console.log(urunler)
 
 console.log(window.location.href)
+console.log(sepet.length)
 
 if (window.location.href == 'http://127.0.0.1:5500/index.html') {
     const row = document.querySelector('.row')
 
     urunler.forEach((urun) => {
+
+        // console.log(urun)
+
         urun.quantity = 1
+
+        // console.log(urun)
 
         const col = document.createElement('div')
         col.classList.add('col-lg-3', 'col-sm-6', 'col-12')
 
         const parentDiv = document.createElement('div')
         parentDiv.style.width = '100%'
-        parentDiv.style.height = '220px'
-        parentDiv.style.border = '1px solid black'
+        parentDiv.style.height = '420px'
+        parentDiv.style.border = "1px solid black"
 
         const imgDiv = document.createElement('div')
-        imgDiv.style.width = '100%'
-        imgDiv.style.height = '100%'
+        imgDiv.style.width = "100%"
+        imgDiv.style.height = "200px"
 
         const img = document.createElement('img')
         img.src = urun.fotoğraf
-        img.style.width = '100%'
-        img.style.height = '100%'
+        img.style.width = "100%"
+        img.style.height = "100%"
 
         const cardBody = document.createElement('div')
-        cardBody.style.width = '100%'
-        cardBody.style.height = '100%'
+        cardBody.style.width = "100%"
+        cardBody.style.height = "220px"
 
         const baslik = document.createElement('h5')
         baslik.textContent = urun.isim
@@ -53,13 +60,33 @@ if (window.location.href == 'http://127.0.0.1:5500/index.html') {
         btn.textContent = 'Sepete Ekle'
 
         btn.addEventListener('click', () => {
-            sepet.push(urun)
+
+            console.log(sepet.length)
+
+            let found = false
+
+            if (sepet.length == 0) {
+                sepet.push(urun)
+
+                found = true
+            } else {
+                sepet.forEach(sepetUrunu => {
+                    // console.log(sepetUrunu.isim == urun.isim)
+                    if (sepetUrunu.isim == urun.isim) {
+                        sepetUrunu.quantity++
+                        found = true
+                    }
+
+                })
+            }
+
+            if (!found) {
+                sepet.push(urun)
+            }
+
             localStorage.setItem('sepet', JSON.stringify(sepet))
             span.textContent = sepet.length
-
-
         })
-
 
         cardBody.append(baslik)
         cardBody.append(aciklama)
@@ -74,48 +101,43 @@ if (window.location.href == 'http://127.0.0.1:5500/index.html') {
 
         row.append(col)
 
-    });
-
-}
-else if (window.location.href == 'http://127.0.0.1:5500/sepet.html') {
+    })
+} else if (window.location.href == 'http://127.0.0.1:5500/sepet.html') {
     const container = document.querySelector('.container')
-
 
     if (sepet.length == 0) {
         const h4 = document.createElement('h4')
-        h4.textContent = 'Sepetinizde ürün yok'
+        h4.textContent = 'Sepetinizde Ürün Yok...'
 
         container.append(h4)
-    }
-    else {
-
+    } else {
+        //! Sepetimin uzunluğu 0 dan büyük olduğu sürece burdaki kodlar çalışacak
         sepet.forEach(urun => {
             const div = document.createElement('div')
-            div.style.width = '100%'
-            div.style.height = '150px'
-            div.style.border = '1px solid black'
-            div.classList.add('d-flex', 'align-items-center', 'justify-content-between',
-            'mt-2')
+            div.style.width = "100%"
+            div.style.height = '200px'
+            div.style.border = "1px solid black"
+            div.classList.add('d-flex', 'align-items-center', 'justify-content-between', 'mt-2')
 
             const imgDiv = document.createElement('div')
-            imgDiv.style.width = '25%'
-            imgDiv.style.height = '100%'
+            imgDiv.style.width = "25%"
+            imgDiv.style.height = "100%"
 
             const img = document.createElement('img')
             img.src = urun.fotoğraf
-            img.style.width = '100%'
-            img.style.height = '100%'
-
+            img.style.width = "100%"
+            img.style.height = "100%"
 
             const baslik = document.createElement('h3')
             baslik.textContent = urun.isim
 
             const price = document.createElement('p')
-            price.textContent = urun.fiyat + `$`
+            let urununFiyati = urun.fiyat * urun.quantity
+            price.textContent = urununFiyati + '$'
             price.style.fontWeight = 'bold'
 
             const kacTane = document.createElement('div')
-            // kacTane.style.width = '50px'
+            //? bir şeye ihtiyaç olursa ekleriz
             kacTane.classList.add('d-flex', 'gap-3')
 
             const sayi = document.createElement('p')
@@ -123,36 +145,46 @@ else if (window.location.href == 'http://127.0.0.1:5500/sepet.html') {
             sayi.classList.add('mt-3', 'fs-4')
 
             const azaltBtn = document.createElement('button')
-            azaltBtn.textContent = '-'
+            azaltBtn.textContent = "-"
             azaltBtn.classList.add('btn', 'btn-light')
 
             const arttirBtn = document.createElement('button')
-            arttirBtn.textContent = '+'
+            arttirBtn.textContent = "+"
             arttirBtn.classList.add('btn', 'btn-light')
 
             azaltBtn.addEventListener('click', function () {
-                if (urun.quantity >=1 ) {
+                if (urun.quantity >= 1) {
                     urun.quantity--
                     sayi.textContent = urun.quantity
 
-                }if (urun.quantity == 0 ) {
-                    // console.log(this);
-                    this.parentElement.parentElement.remove()
 
-                    let urunIndex = sepet.indexOf(urun)
-                    sepet.splice(urunIndex, 1)
-                    localStorage.setItem('sepet', JSON.stringify(sepet))
+                    if (urun.quantity == 0) {
+                        // console.log(this.parentElement.parentElement)
+                        this.parentElement.parentElement.remove()
 
-                    // console.log(sepet.splice);
+                        //! LocalStorage'dan silme
+                        let urunIndex = sepet.indexOf(urun)
+
+                        sepet.splice(urunIndex, 1)
+
+                        localStorage.setItem('sepet', JSON.stringify(sepet))
+                        //! LocalStorage'dan silme
+
+                    }
                 }
-
             })
+
             arttirBtn.addEventListener('click', () => {
                 urun.quantity++
                 sayi.textContent = urun.quantity
+
+                // urun.fiyat += urun.quantity
+                // price.textContent = urun.fiyat
+
             })
 
             imgDiv.append(img)
+
             kacTane.append(azaltBtn)
             kacTane.append(sayi)
             kacTane.append(arttirBtn)
@@ -166,8 +198,7 @@ else if (window.location.href == 'http://127.0.0.1:5500/sepet.html') {
 
         })
 
-
-
     }
-}
 
+}
+// localStorage.clear()
